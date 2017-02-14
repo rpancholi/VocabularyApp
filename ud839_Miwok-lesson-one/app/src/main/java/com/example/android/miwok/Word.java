a5aa1bb6d852c7c9552b4892c4432f3a1df58e89
Rupesh Pancholi <rpancholi15@gmail.com>
2017-02-13 22:27:16 -0600

    Add numbers vocab words
    Number vocab words for the Miwok language

---
+++ ud839_Miwok-lesson-one/app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,32 @@
+package com.example.android.miwok;
+
+/**
+ * Created by Rupesh on 2/13/2017.
+ */
+
+public class Word {
+
+    private String defaultTranslation;
+    private String miwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation){
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return miwokTranslation;
+    }
+
+    public void setMiwokTranslation(String miwokTranslation) {
+        this.miwokTranslation = miwokTranslation;
+    }
+
+    public String getDefaultTranslation() {
+        return defaultTranslation;
+    }
+
+    public void setDefaultTranslation(String defaultTranslation) {
+        this.defaultTranslation = defaultTranslation;
+    }
+}
